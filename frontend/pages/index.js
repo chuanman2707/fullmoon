d9ba51979005ed3766b7cbb836904df553e79ccb
chuanman2707 <chuanman2707@gmail.com>
2019-04-18 17:05:12 +0700

    update backend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,15 @@
-export default () => (
-    <div>Hello JS</div>
-)
+import { Button, Alert } from "reactstrap";
+import Layout from '../components/Layout';
+export default() => {
+    return (
+        <Layout>
+            <div>
+                <Alert color="primary">
+                    Hello project is strapi-next with bootstrap 
+                </Alert>
+                &nbsp; <Button color="primary">Hello from nextjs</Button>
+
+            </div>
+        </Layout>
+    )
+}
